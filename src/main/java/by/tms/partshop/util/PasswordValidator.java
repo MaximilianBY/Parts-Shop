0b7cec49d26519411818428c2d8d1ac6d9ce973f
Maximilian <hitch2290@gmail.com>
2022-08-23 22:55:49 +0300

    Changes by imtos

--- src/main/java/by/tms/partshop/util/PasswordValidator.java
+++ src/main/java/by/tms/partshop/util/PasswordValidator.java
@@ -7,7 +7,7 @@ import javax.validation.ConstraintValidatorContext;
 
 public class PasswordValidator implements ConstraintValidator<PasswordConstraint, String> {
 
-  private static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()â€\"[{}]:;',?/*~$^+=<>]).{6,20}$";
+  private static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()â€\"[{}]:;',?~$^=<>]).{8,20}$";
   private static final Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
 
   @Override
